e911a6887f4c6ed0f001b06ade5cee72a68e23d2
DanielBA05 <luisdanielba3@gmail.com>
2025-05-22 17:32:37 -0600

    Inicio

---
+++ Scrabble/src/main/java/com/mycompany/scrabble/Scrabble.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.scrabble;
+
+/**
+ *
+ * @author Daniel Barboza
+ */
+public class Scrabble {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
